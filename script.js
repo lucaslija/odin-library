b70b562c1d13d45d8953bff2f5dc440f3a56d419
lucaslija <81848948+lucaslija@users.noreply.github.com>
2024-01-21 18:01:31 -0800

    began js scaffolding

---
+++ script.js
@@ -0,0 +1,17 @@
+// Data Structures
+
+  // book constructor
+
+  // library constructor
+function Library() {
+  this.books = [];
+}
+    // library methods
+      // addbook (via books.push)
+      // removeBook
+      // getBook
+      // isInLibrary
+
+const library = new Library();
+
+// User Interface
